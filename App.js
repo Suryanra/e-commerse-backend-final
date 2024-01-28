f75a8b34a6b08138755dc1c3925ed1ad221c83d3
Suryanra <142992104+Suryanra@users.noreply.github.com>
2024-01-28 22:51:43 +0530

    Update App.js

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ app.get('/trial',async(req,resp)=>{
       const data=await model.find({});
       console.log(data);
       // const newdata=data.json();
-      resp.send({'message':"changed"});
+      resp.send({'message':"changed again"});
       // resp.send(data);
 })
 
